4be48da165313b38a4e30f67b81f52dd9a5f469b
Ivan Skiridomov <ivan.skiridomov@gmail.com>
2019-08-15 22:58:24 +0300

    Roman Init (#1)
    * Roman Init
    
    * Roman decoder

---
+++ roman/roman_test.go
@@ -0,0 +1,32 @@
+package roman
+
+import (
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	type args struct {
+		roman string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"XXI",args{"XXI"},21},
+		{"I",args{"I"},1},
+		{"IV",args{"IV"},4},
+		{"MMVIII",args{"MMVIII"},2008},
+		{"MDCLXVI",args{"MDCLXVI"},1666},
+		{"XXI",args{"XXI"},21},
+		{"MCMXC",args{"MCMXC"},1990},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Decode(tt.args.roman); got != tt.want {
+				t.Errorf("Decode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
